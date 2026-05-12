b41d3333b628a0c56b50800d173e9b3e00787841
Rafael427000 <rmirandaagreda@gmail.com>
2026-05-11 22:45:49 -0600

    Add polynomial data structures in main.c

--- src/main.c
+++ src/main.c
@@ -1,2 +1,21 @@
 #include <stdio.h>
 #include "polinomio.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+typedef struct Termino {
+    float coeficiente;
+    int exponente;
+} Term;
+
+typedef struct Nodo {
+    Term termino;
+    struct Nodo* siguiente;
+    struct Nodo* anterior;
+} Nodo;
+
+typedef struct {
+    Nodo* cabeza;
+    Nodo* cola;
+} Polinomio;
